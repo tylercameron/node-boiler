40917a1de51880941e9c46a6c0ae2afca137555c
Tyler <tcameronsmith@gmail.com>
2017-12-29 21:37:11 +1100

    test store form, add dummy data

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -143,11 +143,11 @@ function autocomplete(input, latInput, lngInput) {
     if (!input) return; // skip this fn from running if there is not input on the page
     var dropdown = new google.maps.places.Autocomplete(input);
 
-    // dropdown.addListener("place_changed", () => {
-    //     const place = dropdown.getPlace();
-    //     latInput.value = place.geometry.location.lat();
-    //     lngInput.value = place.geometry.location.lng();
-    // });
+    dropdown.addListener("place_changed", function () {
+        var place = dropdown.getPlace();
+        latInput.value = place.geometry.location.lat();
+        lngInput.value = place.geometry.location.lng();
+    });
     // if someone hits enter on the address field, don't submit the form
     input.on("keydown", function (e) {
         if (e.keyCode === 13) e.preventDefault();
