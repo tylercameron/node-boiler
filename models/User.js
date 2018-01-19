d9b55cf5db96ed94e9a235d5bb8a7b205a1f99c2
Tyler <tcameronsmith@gmail.com>
2018-01-19 21:23:26 +1100

    add email, name and gravatar

--- models/User.js
+++ models/User.js
@@ -1,33 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
-// const md5 = require('md5');
-// const { isEmail } = require('validator');
-const mongodbErrorHandler = require('mongoose-mongodb-errors');
-// const passportLocalMongoose = require('passport-local-mongoose');
+const md5 = require('md5');
+const { isEmail } = require('validator');
+// const mongodbErrorHandler = require('mongoose-mongodb-errors');
 
 const userSchema = new Schema({
-    googleId: String
-    // email: {
-    //     type: String,
-    //     unique: true,
-    //     lowercase: true,
-    //     trim: true,
-    //     validate: { 
-    //         isAsync: true,
-    //         validator: isEmail, 
-    //         message: 'Invalid email.' 
-    //     },
-    //     required: 'Please Supply an email address'
-    // },
-    // name: {
-    //     type: String,
-    //     required: 'Please supply a name',
-    //     trim: true
-    // }
+    googleId: String,
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: { 
+            isAsync: true,
+            validator: isEmail, 
+            message: 'Invalid email.' 
+        },
+        required: 'Please Supply an email address'
+    },
+    name: {
+        type: String,
+        required: 'Please supply a name',
+        trim: true
+    }
 });
 
-// userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.virtual('gravatar').get(function () {
+    const hash = md5(this.email);
+    return `https://gravatar.com/avatar/${hash}?s=200`;
+});
+
 // userSchema.plugin(mongodbErrorHandler);
 
 module.exports = mongoose.model('User', userSchema);
