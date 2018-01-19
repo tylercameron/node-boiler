6b67c9e8c2019736352c420c24ea75348e53d753
Tyler <tcameronsmith@gmail.com>
2018-01-19 21:22:01 +1100

    add user to create store routes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -47,16 +47,23 @@ exports.resize = async (req, res, next) => {
 };
 
 exports.createStore = async (req, res) => {
-    // res.json(req.body);
-    const store = await new Store(req.body).save();
-    res.redirect('/');
+    req.body.author = req.user._id;
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
+};
+
+const confirmOwner = (store, user) => {
+    if (!store.author.equals(user.id)) {
+        throw Error('You must own a store in order to edit it.')
+    }
 };
 
 exports.editStore = async (req, res) => {
     // 1. Find the store given the ID
     const store = await Store.findOne({ _id: req.params.id });
     // 2. confirm they are the owner of the store
-    // TODO
+    confirmOwner(store, req.user);
     // 3. Render out the edit form so the user can update their store
     res.render('editStore', { title: `Edit ${store.name}`, store });
 };
