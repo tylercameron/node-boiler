0e631856a4f56b3ccadc2516f5984b95c4c4a722
Tyler <tcameronsmith@gmail.com>
2018-01-19 21:21:36 +1100

    add logout and isloggedin routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,6 @@ const passport = require('passport');
 
 exports.login = passport.authenticate('google', {
     scope: ['profile', 'email']
-//     failureRedirect: '/login',
-//     failureFlash: 'Failed Login!',
-//     successRedirect: '/',
-//     successFlash: 'You are now logged in!'
 });
 
 exports.googleAuth = passport.authenticate('google', { 
@@ -15,6 +11,17 @@ exports.googleAuth = passport.authenticate('google', {
     successFlash: 'You are now logged in!'
 });
 
-// app.get('/auth/google', passport.authenticate('google', {
-//     scope: ['profile', 'email']
-// }));
+exports.logout = (req, res) => {
+    req.logout();
+    req.flash('success', 'You are now logged out');
+    res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next(); //carry on they are logged in
+        return
+    }
+    req.flash('error', 'Oops you must be logged in to do that');
+    res.redirect('/login');
+};
