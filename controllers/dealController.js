0f060ba1dcfc7c47c9b77d6fdae8ea3c41ba94fb
Tyler <tcameronsmith@gmail.com>
2018-02-15 22:15:50 +1100

    create deal now checks for store, add new store, deal and slug for deal

--- controllers/dealController.js
+++ controllers/dealController.js
@@ -2,6 +2,19 @@ const mongoose = require('mongoose');
 const Deal = mongoose.model('Deal');
 const Store = mongoose.model('Store');
 const User = mongoose.model('User');
+const Category = mongoose.model('Category');
+const slug = require('slugs');
+
+async function makeSlug(store, description) {
+    let dealSlug = slug(store.concat(" ", description));
+    
+    const slugRegExp = new RegExp(`^(${dealSlug})((-[0-9]*$)?)$`, "i");
+    const dealsWithSlug = await Deal.find({ slug: slugRegExp });
+    if (dealsWithSlug.length) {
+        dealSlug = `${dealSlug}-${dealsWithSlug.length + 1}`;
+    }
+    return dealSlug;
+};
 
 exports.getAllDeals = async (req, res) => {
     const deals = await Deal.find();
@@ -9,62 +22,50 @@ exports.getAllDeals = async (req, res) => {
     res.render('deals', { title: 'Deals', deals });
 };
 
-exports.addDeal = (req, res) => {
-    res.render('editDeal', { title: 'Add Deal' });
+exports.addDeal = async (req, res) => {
+    const categories = await Category.find({});
+    res.render('editDeal', { title: 'Add Deal', categories });
 };
 
 exports.createDeal = async (req, res) => {
-    // req.body.author = req.user._id;
-    // console.log(req);
-    // req.body.store = req.params.id;
-    // const store = await Store.findOne({ _id: req.params.id });
-    // req.body.storeName = store.name;
-    // const newDeal = new Deal(req.body);
-    // await newDeal.save();
-    // req.flash('success', 'Deal Saved!');
-    // res.redirect('back');
-    // res.send(req);
-
     // Save user ID
     req.body.author = req.user._id;
+    
+    const storePromise = Store.findOne({ 
+        'location.address': req.body.location.address,
+        'name': req.body.name
+    });
+    const categoryPromise = Category.findOne({ 'category': req.body.category });
+    const [ store, category ] = await Promise.all([ storePromise, categoryPromise ]);
 
-    
-    // const tagPromise = Store.getTagsList();
-    // const storesPromise = Store.find({ tags: tagQuery });
-    // const [tags, stores] = await Promise.all([tagPromise, storesPromise]);
-    
-    // const userPromise = User.find({ _id: req.user.id });
-    const store = await Store.findOne({ 'location.address': req.body.location.address });
-    // const [user, store] = await Promise.all([ userPromise, storePromise ]);
+    if (!category) {
+        req.flash('error', 'You must select a valid category.');
+        res.redirect('back');
+    }
+    req.body.category = category._id;
     
     // Check to see if store already exists
-    if (store) {
-        // TODO Check to see if user has vendor status AND user ID == store.user ID
-        if (req.user.id === store.author.toString()) {
-            // if yes -> set deal verified to TRUE
-            req.body.verified = true;
-        } else {
-            // if no -> set deal verified to FALSE, set Store vendor to FALSE
-            req.body.verified = false;
-        }
-        console.log('store exists');
-        req.body.store = store._id;
-        // req.body.author = req.user._id;
-    } else {
-        console.log('no store in db');
-        // TODO: add default image for store created this way
-        const newStore = await (new Store(req.body)).save();
-    }
         // if yes -> add deal with restaurant ID
         // if no -> create Restaurant - Save It
-    // Select existing Category
-        // if doesnt exist -> create new Category
-    console.log(req.body, req.user, store);
-    res.send(req.body);
-};
+    if (store) {
+        // TODO Check to see if user has vendor status AND user ID == store.user ID
+            // if yes -> set deal verified to TRUE
+            // if no -> set deal verified to FALSE, set Store vendor to FALSE
+        if (req.user.id === store.author.toString()) {
+            req.body.verified = true;
+        } else {
+            req.body.verified = false;
+        }
+    } else {
+        // TODO: add default image for store created this way
+        const newStore = await (new Store(req.body)).save();
+        req.body.store = newStore._id;
+    }
+    const slugs = await makeSlug(req.body.name, req.body.description);
+    req.body.slug = slugs;
+    req.body.store = store._id;
  
-    // exports.getStores = async (req, res) => {
-    //     // console.log(req.name);
-    //     const stores = await Store.find();
-    //     res.render('stores', { title: 'Stores', stores });
-    // };
+    const newDeal = await (new Deal(req.body)).save();
+    req.flash('success', 'Deal Saved!');
+    res.redirect('/deals');
+};
