56a8346df3980486ae55916d2b6845d984d8854e
Tyler <tcameronsmith@gmail.com>
2018-03-13 21:12:20 +1100

    add stripe billing routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,8 @@
+const keyPublishable = process.env.STRIPE_PUBLIC_KEY;
+const keySecret = process.env.STRIPE_SECRET_KEY;
+
+const stripe = require("stripe")(keySecret);
+
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const promisify = require('es6-promisify');
@@ -8,4 +13,25 @@ exports.loginForm = (req, res) => {
 
 exports.account = (req, res) => {
     res.render('account', { title: 'Edit your account' });
+};
+
+exports.stripe = (req, res) => {
+    res.render('stripe', { title: 'Vendor Registration', keyPublishable });
+};
+
+exports.payment = async (req, res) => {
+    const amount = 500;
+
+    const customer = await stripe.customers.create({
+        email: req.body.stripeEmail,
+        source: req.body.stripeToken
+    });
+    console.log(customer);
+    const charge = await stripe.charges.create({
+        amount,
+        description: "Sample Charge",
+        currency: "usd",
+        customer: customer.id
+    });
+    res.render('charge');
 };
