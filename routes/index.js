3a4845413b5987b0f51fe780d768dfec1aa530d9
Tyler <tcameronsmith@gmail.com>
2018-03-14 20:29:59 +1100

    add user auth checks for category and store forms routes

--- routes/index.js
+++ routes/index.js
@@ -7,32 +7,42 @@ const dealController = require('../controllers/dealController');
 const categoryController = require('../controllers/categoryController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
+const { userIsVendor } = require('../handlers/userVerification');
+const { userIsAdmin } = require('../handlers/userVerification');
 
 // router.get('/', storeController.getStores);
 router.get('/', dealController.getAllDeals);
 
-router.get('/add-store', authController.isLoggedIn, storeController.addStore);
+// =======   STORES   ======= //
+router.get('/add-store', 
+    authController.isLoggedIn, 
+    userIsVendor, 
+    storeController.addStore
+);
 router.post('/add-store', 
     storeController.upload,
     catchErrors(storeController.resize),
     catchErrors(storeController.createStore)
 );
-router.post('/add/:id',
+router.post('/add-store/:id',
     storeController.upload,
     catchErrors(storeController.resize),
     catchErrors(storeController.updateStore)
 );
-
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
+// =======   USERS   ======= //
 router.get('/login', userController.loginForm);
 router.get('/auth/google', authController.login);
 router.get('/auth/google/callback', authController.googleAuth);
 router.get('/logout', authController.logout);
 router.get('/account', authController.isLoggedIn, userController.account);
+router.get('/vendor-signup', authController.isLoggedIn, userController.stripe);
+router.post('/api/stripe', userController.payment);
 
+// =======   DEALS   ======= //
 router.get('/deals', catchErrors(dealController.getAllDeals));
 router.get('/deals/:day', catchErrors(dealController.getAllDeals));
 router.get('/deal/:deal', dealController.getDeal);
@@ -52,10 +62,19 @@ router.post('/add-deal/:id',
     catchErrors(dealController.updateDeal)
 );
 
+// =======   CATEGORIES   ======= //
 // router.get('/categories', catchErrors(categoryController.getCategories));
 router.get('/categories', catchErrors(dealController.getDealsByCategory));
+router.get('/categories/add', 
+    authController.isLoggedIn, 
+    userIsAdmin, 
+    categoryController.addCategories
+);
 router.get('/categories/:category', catchErrors(dealController.getDealsByCategory));
-router.get('/categories/add', authController.isLoggedIn, categoryController.addCategories);
-router.post('/categories/add', authController.isLoggedIn, catchErrors(categoryController.createCategories));
+router.post('/categories/add', 
+    authController.isLoggedIn, 
+    userIsAdmin, 
+    catchErrors(categoryController.createCategories)
+);
 
 module.exports = router;
